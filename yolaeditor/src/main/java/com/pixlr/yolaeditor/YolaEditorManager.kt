9e4619eaf1fa846c0423badd117f93885470a645
Adib Surani <muhamad.adib@123rf.com>
2019-11-25 14:50:01 +0800

    initial commit

---
+++ yolaeditor/src/main/java/com/pixlr/yolaeditor/YolaEditorManager.kt
@@ -0,0 +1,16 @@
+package com.pixlr.yolaeditor
+
+class YolaEditorManager {
+
+    companion object {
+
+        @JvmStatic
+        fun getSampleValue(value: String): String {
+            return "Hello $value"
+        }
+
+
+    }
+
+
+}
